62751f589a9ade7983f335bb1564d0ea4dc63e15
Zapharaos <matthieu.freitag@gmail.com>
2025-02-03 14:51:34 +0100

    feat(tests): pkg/env - missing cases

--- pkg/env/env_test.go
+++ pkg/env/env_test.go
@@ -1,6 +1,7 @@
 package env
 
 import (
+	"fmt"
 	"os"
 	"testing"
 	"time"
@@ -10,21 +11,40 @@ import (
 // It verifies that the function returns the correct value for an existing key
 // and the default value for a non-existent key.
 func TestGetString(t *testing.T) {
-	t.Run("Existing key", func(t *testing.T) {
-		// Set an environment variable
-		os.Setenv("TEST_STRING", "value")
-		defer os.Unsetenv("TEST_STRING")
+	t.Run("Non-existent key", func(t *testing.T) {
+		fallback := "default"
 
-		// Check if GetString returns the correct value
-		if val := GetString("TEST_STRING", "default"); val != "value" {
-			t.Errorf("Expected 'value', got '%s'", val)
+		// Check if GetString returns the default value
+		if val := GetString("NON_EXISTENT", fallback); val != fallback {
+			t.Errorf("Expected '%s', got '%s'", fallback, val)
 		}
 	})
 
-	t.Run("Non-existent key", func(t *testing.T) {
-		// Check if GetString returns the default value
-		if val := GetString("NON_EXISTENT", "default"); val != "default" {
-			t.Errorf("Expected 'default', got '%s'", val)
+	t.Run("Unexpected type", func(t *testing.T) {
+		fallback := "default"
+		value := "123"
+
+		// Set an environment variable to an unexpected type
+		os.Setenv("TEST_STRING", value)
+		defer os.Unsetenv("TEST_STRING")
+
+		// Check if GetString returns the correct value
+		if val := GetString("TEST_STRING", fallback); val != value {
+			t.Errorf("Expected '%s', got '%s'", value, val)
+		}
+	})
+
+	t.Run("Existing key", func(t *testing.T) {
+		fallback := "default"
+		value := "value"
+
+		// Set an environment variable
+		os.Setenv("TEST_STRING", value)
+		defer os.Unsetenv("TEST_STRING")
+
+		// Check if GetString returns the correct value
+		if val := GetString("TEST_STRING", fallback); val != value {
+			t.Errorf("Expected '%s', got '%s'", value, val)
 		}
 	})
 }
@@ -33,21 +53,40 @@ func TestGetString(t *testing.T) {
 // It verifies that the function returns the correct integer value for an existing key
 // and the default integer value for a non-existent key.
 func TestGetInt(t *testing.T) {
-	t.Run("Existing key", func(t *testing.T) {
-		// Set an environment variable
-		os.Setenv("TEST_INT", "42")
-		defer os.Unsetenv("TEST_INT")
+	t.Run("Non-existent key", func(t *testing.T) {
+		fallback := 123
 
-		// Check if GetInt returns the correct integer value
-		if val := GetInt("TEST_INT", 0); val != 42 {
-			t.Errorf("Expected 42, got %d", val)
+		// Check if GetInt returns the default integer value
+		if val := GetInt("NON_EXISTENT", fallback); val != fallback {
+			t.Errorf("Expected %d, got %d", fallback, val)
 		}
 	})
 
-	t.Run("Non-existent key", func(t *testing.T) {
-		// Check if GetInt returns the default integer value
-		if val := GetInt("NON_EXISTENT", 0); val != 0 {
-			t.Errorf("Expected 0, got %d", val)
+	t.Run("Unexpected type", func(t *testing.T) {
+		fallback := 123
+		value := "not_an_int"
+
+		// Set an environment variable to an unexpected type
+		os.Setenv("TEST_INT", value)
+		defer os.Unsetenv("TEST_INT")
+
+		// Check if GetInt returns the default value
+		if val := GetInt("TEST_INT", fallback); val != fallback {
+			t.Errorf("Expected %d, got %d", fallback, val)
+		}
+	})
+
+	t.Run("Existing key", func(t *testing.T) {
+		fallback := 123
+		value := 42
+
+		// Set an environment variable
+		os.Setenv("TEST_INT", fmt.Sprintf("%d", value))
+		defer os.Unsetenv("TEST_INT")
+
+		// Check if GetInt returns the correct integer value
+		if val := GetInt("TEST_INT", fallback); val != value {
+			t.Errorf("Expected %d, got %d", value, val)
 		}
 	})
 }
@@ -56,21 +95,40 @@ func TestGetInt(t *testing.T) {
 // It verifies that the function returns the correct boolean value for an existing key
 // and the default boolean value for a non-existent key.
 func TestGetBool(t *testing.T) {
-	t.Run("Existing key", func(t *testing.T) {
-		// Set an environment variable
-		os.Setenv("TEST_BOOL", "true")
-		defer os.Unsetenv("TEST_BOOL")
+	t.Run("Non-existent key", func(t *testing.T) {
+		fallback := true
 
-		// Check if GetBool returns the correct boolean value
-		if val := GetBool("TEST_BOOL", false); val != true {
-			t.Errorf("Expected true, got %v", val)
+		// Check if GetBool returns the default boolean value
+		if val := GetBool("NON_EXISTENT", fallback); val != fallback {
+			t.Errorf("Expected %t, got %t", fallback, val)
 		}
 	})
 
-	t.Run("Non-existent key", func(t *testing.T) {
-		// Check if GetBool returns the default boolean value
-		if val := GetBool("NON_EXISTENT", false); val != false {
-			t.Errorf("Expected false, got %v", val)
+	t.Run("Unexpected type", func(t *testing.T) {
+		fallback := true
+		value := "not_a_bool"
+
+		// Set an environment variable to an unexpected type
+		os.Setenv("TEST_BOOL", value)
+		defer os.Unsetenv("TEST_BOOL")
+
+		// Check if GetBool returns the default value
+		if val := GetBool("TEST_BOOL", fallback); val != fallback {
+			t.Errorf("Expected %t, got %t", fallback, val)
+		}
+	})
+
+	t.Run("Existing key", func(t *testing.T) {
+		fallback := true
+		value := false
+
+		// Set an environment variable
+		os.Setenv("TEST_BOOL", fmt.Sprintf("%t", value))
+		defer os.Unsetenv("TEST_BOOL")
+
+		// Check if GetBool returns the correct boolean value
+		if val := GetBool("TEST_BOOL", fallback); val != value {
+			t.Errorf("Expected %t, got %t", value, val)
 		}
 	})
 }
@@ -79,22 +137,40 @@ func TestGetBool(t *testing.T) {
 // It verifies that the function returns the correct duration value for an existing key
 // and the default duration value for a non-existent key.
 func TestGetDuration(t *testing.T) {
-	t.Run("Existing key", func(t *testing.T) {
-		// Set an environment variable
-		os.Setenv("TEST_DURATION", "1h")
-		defer os.Unsetenv("TEST_DURATION")
+	t.Run("Non-existent key", func(t *testing.T) {
+		fallback := 5 * time.Second
 
-		// Check if GetDuration returns the correct duration value
-		expected := time.Hour
-		if val := GetDuration("TEST_DURATION", 0); val != expected {
-			t.Errorf("Expected %v, got %v", expected, val)
+		// Check if GetDuration returns the default duration value
+		if val := GetDuration("NON_EXISTENT", fallback); val != fallback {
+			t.Errorf("Expected %s, got %s", fallback, val)
 		}
 	})
 
-	t.Run("Non-existent key", func(t *testing.T) {
-		// Check if GetDuration returns the default duration value
-		if val := GetDuration("NON_EXISTENT", 0); val != 0 {
-			t.Errorf("Expected 0, got %v", val)
+	t.Run("Unexpected type", func(t *testing.T) {
+		fallback := 5 * time.Second
+		value := "not_a_duration"
+
+		// Set an environment variable to an unexpected type
+		os.Setenv("TEST_DURATION", value)
+		defer os.Unsetenv("TEST_DURATION")
+
+		// Check if GetDuration returns the default value
+		if val := GetDuration("TEST_DURATION", fallback); val != fallback {
+			t.Errorf("Expected %s, got %s", fallback, val)
+		}
+	})
+
+	t.Run("Existing key", func(t *testing.T) {
+		fallback := 5 * time.Second
+		value := 10 * time.Second
+
+		// Set an environment variable
+		os.Setenv("TEST_DURATION", value.String())
+		defer os.Unsetenv("TEST_DURATION")
+
+		// Check if GetDuration returns the correct duration value
+		if val := GetDuration("TEST_DURATION", fallback); val != value {
+			t.Errorf("Expected %s, got %s", value, val)
 		}
 	})
 }
